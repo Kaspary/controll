f4b6d8ad6c2f49f05464de1bd5a0678d9c46456d
João Pedro Kaspary <joaopedrokaspary@hotmail.com>
2020-02-21 11:16:16 -0300

    commit

--- services/server/controll/core/admin.py
+++ services/server/controll/core/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(SystemUser)
+admin.site.register(DateUpdated)
--- services/server/controll/core/models.py
+++ services/server/controll/core/models.py
@@ -13,7 +13,7 @@ class SystemUser(models.Model):
     earnings = models.ManyToManyField(Earnings)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    last_date_updated = models.DateTimeField(auto_now=True)
+    # last_date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.pk)
